7d4dd9d6e7bf33d1f742e3d57e0ce88787053747
Alrico Rizki <alricosaiyan123@gmail.com>
2023-02-28 20:12:07 +0700

    TUGAS TASK OPERATOR

---
+++ operator.js
@@ -0,0 +1,45 @@
+//SOAL NOMER SATU
+let angka = 1+2+3
+let angka1 = 11+22+33
+let angka2 = 111+222+333
+let angka3 = 1111+2222+3333
+let angka4 = 11111+22222+33333
+console.log("Hasil ",angka)
+console.log("Hasil ",angka1)
+console.log("Hasil ",angka2)
+console.log("Hasil ",angka3)
+console.log("Hasil ",angka4)
+// SOAL NOMER DUA
+
+let perbandingan = 1>2
+let perbandingan1 = 2>1
+let perbandingan2 = 1<2
+let perbandingan3 = 2<1
+let perbandingan4 = 1>=2
+let perbandingan5 = 2>=1
+let perbandingan6 = 1<=2
+let perbandingan7 = 1>=2
+let perbandingan8 = 1==2
+let perbandingan9 = 2==2
+let perbandingan10 = 2 === '2'
+let perbandingan11 = 2 === '2'
+let perbandingan12 = 2 != 2
+let perbandingan13 = 3 != 2
+let perbandingan14 = "2"!=="2"
+let perbandingan15 = "2"!==2
+console.log("Perbandingan 1>2 ",perbandingan)
+console.log("Perbandingan 2>1 ",perbandingan1)
+console.log("Perbandingan 1<2 ",perbandingan2)
+console.log("Perbandingan 2<1 ",perbandingan3)
+console.log("Perbandingan 1>=2 ",perbandingan4)
+console.log("Perbandingan 2>=1 ",perbandingan5)
+console.log("Perbandingan 1<=2 ",perbandingan6)
+console.log("Perbandingan 1>=2 ",perbandingan7)
+console.log("Perbandingan 1==2 ",perbandingan8)
+console.log("Perbandingan 2==2 ",perbandingan9)
+console.log("Perbandingan 2 === '2' ",perbandingan10)
+console.log("Perbandingan  2 === '2' ",perbandingan11)
+console.log("Perbandingan 2 != 2 ",perbandingan12)
+console.log("Perbandingan 3 != 2 ",perbandingan13)
+console.log("Perbandingan '2'!=='2' ",perbandingan14)
+console.log("Perbandingan '2'!==2 ",perbandingan15)
